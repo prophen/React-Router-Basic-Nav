281b88095d07c8638416237c3b37254d05ab8670
Nikema Prophet <nikema@gmail.com>
2019-07-22 12:51:46 -0700

    Setting up Navigation with Link

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => (
   <div>
     <Navigation />
     <Route path="/" exact component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/contact" component={Contact} />
+    <Route path="/about/" component={About} />
+    <Route path="/contact/" component={Contact} />
   </div>
 );
 
