32b7ddbe463d1383efd6af782f1fc865cf63dd30
Nikema Prophet <nikema@gmail.com>
2019-07-22 16:25:41 -0700

    Fixing ending Link tag in Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom'
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </div>
         <div>
           <Link to="/about/">About</Link>
